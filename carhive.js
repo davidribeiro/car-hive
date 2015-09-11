8a98711c9db48d8d2dd2ee8711ded22e02a524e6
prdct <bruno.sum@hotmail.com>
2015-09-11 22:52:14 +0100

    Car Hive Server 0.1
    Refactoring Post, added maxRows to get.

--- carhive.js
+++ carhive.js
@@ -1,36 +1,8 @@
-/*//Lets require/import the HTTP module
-var http = require('http');
-
-//Lets define a port we want to listen to
-const PORT=8080; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-	//Enviar json por ficheiro. (testar com stream?)
-	var jsonFile = '{'
-       +'"name" : "ponto",'
-       +'"points"  : -111,-222,'
-       +'"parkingspots" : 15/16'
-       +'}';
-	response.setHeader('Content-Type', 'application/json');
-    response.send(jsonFile);
-}
-
-
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});*/
-
 var express = require('express');
 var app = express();
 var http = require('http');
 var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
 app.get('/', function (request, response) {
 	console.log('New request from ' + request.ip);
@@ -44,7 +16,19 @@ app.get('/', function (request, response) {
     response.send(jsonFile);
 });
 
-//Adicionar maxrows
+var findUser = function(db, resp){
+	var cursor = db.collection('hiveUsers').find();
+
+	resp(cursor, db.collection('hiveUsers'));
+}
+
+var findStreet = function(db, resp){
+	var cursor = db.collection('hiveStreets').find();
+
+	resp(cursor, db.collection('hiveStreets'));
+}
+
+//Adicionar maxrows [TESTE: Working]
 app.get(/lat=-?\d*.\d*&lng=-?\d*.\d*&maxRows=\d*/, function (request, response){
 	console.log('New map request from ' + request.ip);
 
@@ -69,75 +53,93 @@ app.get(/lat=-?\d*.\d*&lng=-?\d*.\d*/, function (request, response){
 //Post marker
 app.post(/lat=-?\d*.\d*&lng=-?\d*.\d*&flag=\d*/, function (request, response){
 	console.log('New post markers from ' + request.ip);
-
-	var flag = request.originalUrl.toString().match('&flag=\d*').substring(6);
+	var user = "testadmin";
+	var flag = request.originalUrl.toString().match(/&flag=\d*/).toString().substring(6);
 
 	if(flag == 0){
 		//Localização carro estacionado
+		//
 		requestGeoNames(request.originalUrl ,function(json){
 
-			connectDB(user, function(db){
+			connectDB(function(db){
 				console.log('Novo acesso a base de dados de ' + user);
 
-				var collection = db.collection('carHiveUsers');
-				var cursor = collection.find({"user":""+user+""});
-
-				if(cursor.count() < 1){
-					//Inserir novo utilizador
-					collection.insert({"name":""+user+"","carcoord":""+request.originalUrl.toString().match('lat=-?\d*.\d*').substring(4)+","request.originalUrl.toString().match('lng=-?\d*.\d*').substring(4)+""}, function(err, record){
-						if(err){
-							console.log("Erro ao introduzir novo utilizador");
+				findUser(db, function(cursor, collection){
+					cursor.count(function(err, count){
+						if(count < 1){
+							//Inserir novo user [Test:TODO]
+							//
+							collection.insert({"name":""+user+"","carcoord":""+request.originalUrl.toString().match('lat=-?\d*.\d*').substring(4)+","+request.originalUrl.toString().match('lng=-?\d*.\d*').substring(4)+""}, function(err, record){
+							if(err){
+								console.log("Erro ao introduzir novo utilizador");
+							}else{
+								console.log("Novo utilizador "+record[0].name);
+							}
+							});
+							//
+							////
 						}else{
-							console.log("Novo utilizador "+record[0].name);
+							//Update utilizador existente [Test:TODO]
+							//
+							console.log("Update utilizador");
+							collection.update({"name":""+user+""}, {"carcoord":""+request.originalUrl.toString().match('lat=-?\d*.\d*').substring(4)+","+request.originalUrl.toString().match('lng=-?\d*.\d*').substring(4)+""});
+							//
+							/////
 						}
-					}););
-				}
-				else{
-					//Update utilizador existente
-					collection.update({"name":""+user+""}, {"carcoord":""+request.originalUrl.toString().match('lat=-?\d*.\d*').substring(4)+","request.originalUrl.toString().match('lng=-?\d*.\d*').substring(4)+""});
-				}
+					});
+				});
+
+				
 			});
 			//
 		});
+		//
+		////
 	}
 	else if(flag > 0){
-		//Localização estacionamento
+		//Localização estacionamento [Test:TODO]
+		//
 		requestGeoNames(request.originalUrl, function(json){
-			var street = json.streetSegment[1].name;
+			var street = JSON.parse(json).streetSegment[1].name;
 
-			connectDB(user, function(db){
+			connectDB(function(db){
 				console.log('Novo acesso a base de dados de ' + user);
 
-				var collection = db.collection('carHiveStreets');
-				var subcollection = db.collection('carHiveMarkers');
-				var cursor = collection.find({"name":""+street+""});
+				findStreet(db, function(cursor, collection){
+					var max = JSON.parse(collection.find().sort({"IDmarker": -1}).limit(1)).IDmarker;
+					console.log('Numero de ruas' + max);
+					//TODO
+					if(cursor.count() < 1){
+						//Inserir nova rua
+						var options = { "sort": {IDmarker:-1} };
+						var max = subcollection.findOne({}, options) +1;					
+						console.log(subcollection.findOne({}, options));
 
-				var currentDate = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-				var UTCdate = Date.UTC(currentDate.year, currentDate.month, currentDate.day, currentDate.hours, currentDate.minutes);
+						var currentDate = new Date();
+						var UTCdate = Date.UTC(currentDate.year, currentDate.month, currentDate.day, currentDate.hours, currentDate.minutes);
 
-				if(cursor.count() < 1){
-					//Inserir nova rua
-					var options = { "sort": {IDmarker:-1} };
+						collection.insert({"name":""+street+"","IDmarker":""+max+""}, function(err, record){
+							if(err){
+								console.log("Erro ao introduzir nova rua");
+							}else{
+								console.log("Nova rua "+record[0].name);
+							}
+						});
 
-					var max = subcollection.findOne({}, options) +1;					
+						console.log("nova rua, novo marcador");
+						subcollection.insert({"IDmarker":""+max+"","nSpots":""+flag+"","IDuser":""+user+"","date":""+currentDate.toJSON()+""});
 
-					collection.insert({"name":""+street+"","IDmarker":""+max+""}, function(err, record){
-						if(err){
-							console.log("Erro ao introduzir nova rua");
-						}else{
-							console.log("Nova rua "+record[0].name);
-						}
-					});
-
-					subcollection.insert({"IDmarker":""+max+"","nSpots":""+flag+"","IDuser":""+user+"","date":""+UTCdate+""});
-
-				}
-				else{
-					//Update marcadores rua
-					subcollection.insert({"IDmarker":""+collection.find({"name":""+street+""}).IDmarker+"","nSpots":""+flag+"","IDuser":""+user+"","date":""+UTCdate+""},{});
-				}
+					}
+					else{
+						//Update marcadores rua
+						console.log("update marcador");
+						subcollection.insert({"IDmarker":""+collection.find({"name":""+street+""}).IDmarker+"","nSpots":""+flag+"","IDuser":""+user+"","date":""+UTCdate.toJSON()+""});
+					}
+				});
 			});
 		});
+		//
+		////
 	}else{
 		response.send('get não reconhecido');
 	}
@@ -151,7 +153,7 @@ var server = app.listen(8080, function () {
 });
 
 
-function connectdb(DBaction){
+function connectDB(DBaction){
 	var DBurl = 'mongodb://localhost:27017/carHive';
 	MongoClient.connect(DBurl, function(err, db){
 		if(err){
@@ -159,7 +161,6 @@ function connectdb(DBaction){
 		}
 		else{
 			DBaction(db);
-			db.close();
 		}
 	});
 }
@@ -168,7 +169,7 @@ function connectdb(DBaction){
 function requestGeoNames(coords, endreq){
 	var geonamesAPI = {
 		host: 'api.geonames.org',
-		path: '/findNearbyStreetsOSMJSON?'+coords.toString().substring(1)'&username=carHive',
+		path: '/findNearbyStreetsOSMJSON?'+coords.toString().substring(1)+'&username=carHive',
 	};
 
 	var req = http.request(geonamesAPI, function(res){
